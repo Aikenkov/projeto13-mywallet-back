45634446254d609efef23f504cafd007a34a9e75
Aikenkov <Fabio.rod.silva.spqr@gmail.com>
2022-09-09 17:24:02 -0300

    SignIn implementado

--- src/controllers/participant.controller.js
+++ src/controllers/participant.controller.js
@@ -3,10 +3,15 @@ import joi from "joi";
 import dayjs from "dayjs";
 import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
-//import { ObjectId } from "mongodb";
+import { ObjectId } from "mongodb";
 
 const signupSchema = joi.object({
     name: joi.string().trim().min(1).max(30).required().strict(),
+    email: joi.string().required().email(),
+    password: joi.string().min(4).required(),
+});
+
+const signinSchema = joi.object({
     email: joi.string().required().email(),
     password: joi.string().min(4).required(),
 });
@@ -43,14 +48,73 @@ async function signUp(req, res) {
     const passwordHash = bcrypt.hashSync(password, 10);
 
     try {
-        const participant = await db
+        await db
             .collection("users")
             .insertOne({ name, email, password: passwordHash });
-        res.send(participant);
+        res.sendStatus(200);
     } catch (error) {
         console.log(error.message);
         res.sendStatus(500);
     }
 }
 
-export { signUp };
+async function signIn(req, res) {
+    const { email, password } = req.body;
+
+    const validation = signinSchema.validate(
+        {
+            email,
+            password,
+        },
+        { abortEarly: false }
+    );
+
+    if (validation.error) {
+        const erros = validation.error.details.map(
+            (details) => details.message
+        );
+        return res.status(422).send(erros);
+    }
+
+    const user = await db.collection("users").findOne({ email });
+    if (user === null) {
+        return res.status(400).send({ message: "Email ou senha incorretos" });
+    }
+
+    const passwordIsValid = bcrypt.compareSync(password, user.password);
+
+    if (passwordIsValid) {
+        const token = uuid();
+
+        try {
+            const exist = await db.collection("sessions").findOne({
+                userId: user._id,
+            });
+
+            if (exist) {
+                await db
+                    .collection("sessions")
+                    .updateOne({ userId: user._id }, { $set: { token } });
+                return res.send(token);
+            }
+        } catch (error) {
+            console.log(error.message);
+            res.sendStatus(500);
+        }
+
+        try {
+            await db.collection("sessions").insertOne({
+                userId: user._id,
+                token,
+            });
+        } catch (error) {
+            console.log(error.message);
+            res.sendStatus(500);
+        }
+        res.send(token);
+    } else {
+        res.status(400).send({ message: "Email ou senha incorretos" });
+    }
+}
+
+export { signUp, signIn };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import cors from "cors";
-import { signUp } from "./controllers/participant.controller.js";
+import { signUp, signIn } from "./controllers/participant.controller.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.post("/signUp", signUp);
+app.post("/signIn", signIn);
 
 app.listen(5000, () => console.log("escutando na porta 5000"));
