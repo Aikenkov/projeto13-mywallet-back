11afcdc76569c529875a8b8fc4a0cf9d623cc74d
Aikenkov <Fabio.rod.silva.spqr@gmail.com>
2022-09-10 11:40:00 -0300

    refactor(routers): implementa o uso de routers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import express from "express";
 import cors from "cors";
-import { signUp, signIn } from "./controllers/participant.controller.js";
+import participantRouter from "./routers/participant.router.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post("/signUp", signUp);
-app.post("/signIn", signIn);
+app.use(participantRouter);
 
 app.listen(5000, () => console.log("escutando na porta 5000"));
